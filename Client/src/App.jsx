85f8f6c0b886bc29875b82979a125a86aa4c1a10
mann <jadhavmann.mj@gmail.com>
2024-09-17 18:27:34 +0530

    added Footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,6 +10,7 @@ import Login from './pages/Login';
 import PlaceOrder from './pages/PlaceOrder';
 import Order from './pages/Order';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
@@ -26,6 +27,7 @@ const App = () => {
         <Route path="/Place-order" element={<PlaceOrder />} />
         <Route path="/Order" element={<Order />} /> {/* Fixed casing here */}
       </Routes>
+      <Footer/>
     </div>
   );
 };
