b096af29d4472cec58445150847605b2c67520af
mann <jadhavmann.mj@gmail.com>
2024-09-19 21:27:36 +0530

    updated

---
+++ Client/src/context/components/Navbar.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import { assets } from '../assets/assets';
+import { Link, NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div className='relative flex items-center justify-between py-5 font-medium'>
+
+      {/* Logo */}
+      <img src={assets.logo} className='w-36' alt="logo" />
+
+      {/* Desktop Menu */}
+      <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+        <NavLink to='/' className='flex flex-col items-center gap-1'>
+          <p>HOME</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/collection' className='flex flex-col items-center gap-1'>
+          <p>COLLECTION</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/about' className='flex flex-col items-center gap-1'>
+          <p>ABOUT</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+        <NavLink to='/contact' className='flex flex-col items-center gap-1'>
+          <p>CONTACT</p>
+          <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden' />
+        </NavLink>
+      </ul>
+
+      {/* Icons */}
+      <div className='flex items-center gap-6'>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt="search" />
+
+        {/* Profile Dropdown */}
+        <div className='group relative'>
+          <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="profile" />
+          <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pointer hover:text-black'>My Profile</p>
+              <p className='cursor-pointer hover:text-black'>Order</p>
+              <p className='cursor-pointer hover:text-black'>Logout</p>
+            </div>
+          </div>
+        </div>
+
+        {/* Cart Icon */}
+        <Link to='/cart' className='relative'>
+          <img src={assets.cart_icon} className='w-5 min-w-5' alt="cart" />
+          <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+        </Link>
+
+        {/* Mobile Menu Icon */}
+        <img onClick={toggleMenu} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="menu" />
+      </div>
+
+      {/* Sidebar for Mobile Screens */}
+      <div className={ `fixed top-0 right-0 bottom-0 transition-transform bg-white z-50 ${visible ? 'translate-x-0' : 'translate-x-full'} w-full sm:w-[250px]`}>
+        <div className='flex flex-col text-gray-600'>
+          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+            <img className='h-4 rotate-180' src={assets.dropdown_icon} alt=""/>
+            <p>Back</p>
+          </div>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-5 border' to='/'>HOME</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-5 border' to='/collection'>COLLECTION</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-5 border' to='/about'>ABOUT</NavLink>
+          <NavLink onClick={() => setVisible(false)} className='py-2 pl-5 border' to='/contact'>CONTACT</NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Hero from '../components/Hero';
-import LatestCollection from '../components/LatestCollection';
+import LatestCollection from '../context/components/LatestCollection';
 
 const Home = () => {
   return (
