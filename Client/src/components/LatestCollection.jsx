89d4491d5ea1945a6f81e495a3b237d01b0b8a97
mann <jadhavmann.mj@gmail.com>
2024-09-19 11:19:34 +0530

    completed title

--- Client/src/components/LatestCollection.jsx
+++ Client/src/components/LatestCollection.jsx
@@ -1,14 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {ShopContext} from '../context/ShopContext.jsx'
+import Title from './Title.jsx';
 
 const LatestCollection = () => {
 
     const { products} = useContext(ShopContext);
+    const [latesProducts,setLatestProducts] = useState([]);
 
-    console.log(products);
+    useEffect(() => {
+      setLatestProducts(products.slice(0,10));
+    },[])
+
 
   return (
-    <div>
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={'LATEST'} text2={'COLLECTIONS'}/>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        </p>
+      </div>
       
     </div>
   )
